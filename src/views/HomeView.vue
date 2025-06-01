a2bf48634ab4e0a0599bb55e69a7cf61df8e27f9
Kobeieii <ploy.thitiworada@outlook.com>
2025-06-01 14:56:58 +0700

    fix(app): lint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,8 +22,8 @@
           :auto="true"
           choose-label="Import"
           choose-icon="pi pi-download"
-          @select="onSelectExcel"
           :disabled="disabledImportExcel"
+          @select="onSelectExcel"
         />
         <Button label="New Data" icon="pi pi-plus" size="small" @click="handleAddData" />
       </div>
@@ -135,27 +135,12 @@ async function exportExcel() {
   isLoadingExportExcel.value = false
 }
 
-async function onUploadExcel(event) {
-  const file = event.files[0]
-  if (!file) { return }
-
-  const reader = new FileReader()
-  reader.onload = (e) => {
-    const data = new Uint8Array(e.target.result)
-    const workbook = XLSX.read(data, { type: 'array' })
-    const sheetName = workbook.SheetNames[0]
-    const worksheet = workbook.Sheets[sheetName]
-    const json = XLSX.utils.sheet_to_json(worksheet, { defval: '' })
-
-    console.log('Parsed Excel data:', json)
-  }
-  reader.readAsArrayBuffer(file)
-}
-
 async function onSelectExcel(event) {
   disabledImportExcel.value = true
   const file = event.files?.[0]
-  if (!file) { return }
+  if (!file) {
+    return
+  }
   const success = await dataMappingStore.importExcel(file)
   if (success) {
     toast.add({ severity: 'success', summary: 'Success', detail: 'Data imported successfully', life: 2000 })
